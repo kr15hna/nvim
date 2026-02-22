b2098dccd78733148a66c32fa4f4af2e29dfa1a9
Krishna <krishnasainunna@gmail.com>
2026-02-22 23:13:17 +0530

    nuke nvim-http

--- lua/plugins/nvim-http.lua
+++ lua/plugins/nvim-http.lua
@@ -8,9 +8,9 @@
 --
 --
 return {
-  dir = vim.fn.stdpath("config") .. "/nvim-http",
-  name = "nvim-http",
-  config = function()
-    require("nvim_http").setup()
-  end,
+  -- dir = vim.fn.stdpath("config") .. "/nvim-http",
+  -- name = "nvim-http",
+  -- config = function()
+  --   require("nvim_http").setup()
+  -- end,
 }
