25449b3c227fbeb69d150d9243619dae5db71b31
Krishna <krishnasainunna@gmail.com>
2026-02-22 11:34:45 +0530

    neo-tree simple search

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -18,8 +18,20 @@ return {
       opts = opts or {}
       opts.filesystem = opts.filesystem or {}
       opts.filesystem.hijack_netrw_behavior = "open_default"
+      opts.filesystem.follow_current_file = opts.filesystem.follow_current_file or {}
+      opts.filesystem.follow_current_file.enabled = true
+      opts.filesystem.window = opts.filesystem.window or {}
+      opts.filesystem.window.mappings = opts.filesystem.window.mappings or {}
+      opts.filesystem.window.mappings["/"] = "none"
+      opts.filesystem.window.mappings["Z"] = "expand_all_subnodes"
 
       opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+      opts.filesystem.filtered_items.visible = false
+      opts.filesystem.filtered_items.hide_by_name = {
+        ".git",
+        ".cargo",
+      }
+
       opts.filesystem.filtered_items.hide_dotfiles = false
       opts.filesystem.filtered_items.hide_hidden = false
       opts.filesystem.filtered_items.hide_gitignored = false
